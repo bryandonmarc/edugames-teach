a3e5b9fe622f302ce4a33e95cb7defb6b34a84a2
bryandonmarc <bryan.donmarc@gmail.com>
2021-02-16 05:31:24 +0800

    refactor(layout): add home teacher layout

---
+++ src/layouts/homeTeacherLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex w-full h-screen overflow-hidden select-none">
+    <Navbar />
+    <Nuxt />
+    <Profile />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
